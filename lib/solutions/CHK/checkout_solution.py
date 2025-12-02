02de4b7427009512a6d012aca45b2b3bfd299777
sbx_user1051 <sbx_user1051@169.254.25.151>
2025-12-02 16:32:32 +0000

    Tue Dec 02 16:32:32 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ class CheckoutSolution:
         'D': 15
     }
 
+
     SPECIALS = {
         'A': { 3: 130, 5: 200 },
         'B': { 2: 45 },
@@ -25,26 +26,33 @@ class CheckoutSolution:
         counter = Counter(skus)
         for sku, specials in CheckoutSolution.SPECIALS:
             for num_items, special in specials:
-                if isinstance(special, tuple) and counter[sku]>=num_items:
-                    if
-                    free_items = counter[sku]//num_items
-                    counter[sku]-=free_items
+                # Check if count of items matches free item special
+                if isinstance(special, tuple):
+                    if sku in counter and counter[sku]>=num_items:
+                        free_sku = special[0]
+                        num_free_sku = special[1]
+                        if free_sku in counter:
+                            free_items = (counter[sku]//num_items)*num_free_sku
+                            counter[free_sku]-=free_items
 
         total = 0
         for sku, items in counter.items():
+            # skip neg or 0 item counts - indicates free sku
+            if items<1:
+                continue
             sku_price = CheckoutSolution.PRICING[sku]
             if sku in CheckoutSolution.SPECIALS:
                 specials = CheckoutSolution.SPECIALS[sku]
                 ordered_offers = sorted(specials.keys(), reverse=True)
                 for special_num_items, special_value in ordered_offers.items():
-                    if items>=special_num_items:
-                        if not isinstance(special_value, tuple):
-                            special_price = (items//special_num_items)*special_value
-                            items = items % special_num_items
-                            total+=special_price
+                    if items>=special_num_items and not isinstance(special_value, tuple):
+                        special_price = (items//special_num_items)*special_value
+                        items = items % special_num_items
+                        total+=special_price
                 rem_price = items * sku_price
                 total += rem_price
             else:
                 total+= sku_price*items
         return total
+
 
