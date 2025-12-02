614f26334cf4090006594fa0ac82144258cc74a0
sbx_user1051 <sbx_user1051@169.254.25.151>
2025-12-02 16:02:16 +0000

    Tue Dec 02 16:02:16 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,4 +20,19 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        
+        for sku in skus:
+            if sku not in CheckoutSolution.PRICING:
+                return -1
+
+        counter = Counter(skus)
+        total = 0
+        for sku, items in counter.items():
+            sku_price = CheckoutSolution.PRICING[sku]
+            if sku in CheckoutSolution.SPECIALS and items>=CheckoutSolution.SPECIALS[sku].num_items:
+                special = CheckoutSolution.SPECIALS[sku]
+                special_price = (items//special.num_items)*special.price
+                rem_price = (items % special.num_items)*sku_price
+                total+=special_price+rem_price
+            else:
+                total+= sku_price*items
+        return total
