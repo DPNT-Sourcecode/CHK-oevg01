6fd470fd77cc45d724486718ce4fb2c74aaea67d
sbx_user1051 <sbx_user1051@169.254.25.151>
2025-12-02 15:56:16 +0000

    Tue Dec 02 15:56:16 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,23 @@
 from collections import Counter
+
+class Special:
+    def __init__(self, num_items, price):
+        self.num_items = num_items
+        self.price = price
+
 class CheckoutSolution:
+    PRICING = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    SPECIALS = {
+        'A': Special(3, 130),
+        'B': Special(3, 130)
+    }
 
     # skus = unicode string
     def checkout(self, skus):
-        print(skus)
+        
